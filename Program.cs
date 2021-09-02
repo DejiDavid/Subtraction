bcb3f192dffc7e4ed295f24b389fd52cb3958164
Deji David <dejidavid07@gmail.com>
2021-09-02 14:02:19 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Subtraction {
+    class Program {
+        static void Main (string[] args) {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int results = 0;
+
+            Console.Write ("Please enter your first number: ");
+            firstNumber = int.Parse (Console.ReadLine ());
+
+            Console.Write ("Please enter the second number: ");
+            secondNumber = int.Parse (Console.ReadLine ());
+
+            results = firstNumber - secondNumber;
+            Console.WriteLine ("The answer is " + results);
+        }
+    }
+}
